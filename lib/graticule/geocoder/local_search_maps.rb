f26c4d32f630e1f544b66fe04eb94d0fbcabb7ab
Brandon Keepers <brandon@collectiveidea.com>
2009-10-01 09:37:50 -0400

    Update localsearchmaps geocoder

--- lib/graticule/geocoder/local_search_maps.rb
+++ lib/graticule/geocoder/local_search_maps.rb
@@ -28,16 +28,17 @@ module Graticule #:nodoc:
       end
     
       def check_error(js)
-        raise AddressError, "Empty Response" if js.nil?
-        raise AddressError, 'Location not found' if js =~ /location not found/
+        case js
+        when nil
+          raise AddressError, "Empty Response"
+        when /location not found/
+          raise AddressError, 'Location not found'
+        end
       end
     
       def parse_response(js)
-        returning Location.new do |location|
-          coordinates = js.match(/map.centerAndZoom\(new GPoint\((.+?), (.+?)\)/)
-          location.longitude = coordinates[1].to_f
-          location.latitude = coordinates[2].to_f
-        end
+        coordinates = js.match(/map.centerAndZoom\(new GPoint\((.+?), (.+?)\)/)
+        Location.new(:longitude => coordinates[1].to_f, :latitude => coordinates[2].to_f)
       end
       
     end
