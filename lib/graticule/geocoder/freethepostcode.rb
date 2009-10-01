fd46442f576f44bea00cbd2d8398c801c20ddaaf
Brandon Keepers <brandon@collectiveidea.com>
2009-09-30 23:03:36 -0400

    Clean up freethepostcode geocoder

--- lib/graticule/geocoder/freethepostcode.rb
+++ lib/graticule/geocoder/freethepostcode.rb
@@ -8,7 +8,7 @@ module Graticule #:nodoc:
     #     location = gg.locate 'W1A 1AA'
     #     location.coordinates
     #     #=> [51.52093, -0.13714]
-    class FreeThePostcode < Rest
+    class FreeThePostcode < Base
 
       def initialize
         @url = URI.parse 'http://www.freethepostcode.org/geocode'
@@ -18,30 +18,20 @@ module Graticule #:nodoc:
         get :postcode => postcode
       end
       
-      private
+    private
+
       def prepare_response(response)
-        response
+        response.split("\n")[1]
       end
       
-      def parse_response(txt)
-        lines = split_response(txt)
-        data = lines[1].split
-        Location.new(:latitude => data[0].to_f,
-                     :longitude => data[1].to_f,
-                     :precision => :unknown)
+      def parse_response(response)
+        data = response.split
+        Location.new(:latitude => data[0].to_f, :longitude => data[1].to_f, :precision => :unknown)
       end
 
-      def check_error(txt)
-        lines = split_response(txt)
-        if lines[1].blank?
-          raise AddressError, 'unknown address'
-        end
+      def check_error(response)
+        raise AddressError, 'unknown address' if response.blank?
       end
-      
-      def split_response(txt)
-        txt.map{|line| line}
-      end
-
       
     end
   end
