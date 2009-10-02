30500972a13ea7e9a20a272ec2256da52fddfebb
Brandon Keepers <brandon@collectiveidea.com>
2009-10-02 16:08:51 -0400

    Refactor geocoder_ca to use HappyMapper

--- lib/graticule/geocoder/geocoder_ca.rb
+++ lib/graticule/geocoder/geocoder_ca.rb
@@ -14,29 +14,54 @@ module Graticule #:nodoc:
       end
       
     private
-      
-      def parse_response(xml) #:nodoc:
-        returning Location.new do |location|
-          location.latitude = xml.elements['geodata/latt'].text.to_f
-          location.longitude = xml.elements['geodata/longt'].text.to_f
-          location.street = xml.elements['geodata/standard/staddress'].text
-          location.locality = xml.elements['geodata/standard/city'].text
-          location.region = xml.elements['geodata/standard/prov'].text
+    
+      class Response
+        include HappyMapper
+        tag 'geodata'
+        element :latitude, Float, :tag => 'latt'
+        element :longitude, Float, :tag => 'longt'
+        element :street_number, String, :deep => true, :tag => 'stnumber'
+        element :street_name, String, :deep => true, :tag => 'staddress'
+        element :locality, String, :deep => true, :tag => 'city'
+        element :postal_code, String, :tag => 'postal'
+        element :region, String, :deep => true, :tag => 'prov'
+        
+        class Error
+          include HappyMapper
+          tag 'error'
+          element :code, Integer
+          element :description, String
+        end
+        
+        has_one :error, Error
+        
+        def street
+          [street_number, street_name].join(' ')
         end
       end
+      
+      def prepare_response(xml)
+        Response.parse(xml, :single => true)
+      end
+      
+      def parse_response(response) #:nodoc:
+        Location.new(
+          :latitude  => response.latitude,
+          :longitude => response.longitude,
+          :street    => response.street,
+          :locality  => response.locality,
+          :region    => response.region
+        )
+      end
 
-      def check_error(xml) #:nodoc:
-        error = xml.elements['geodata/error']
-        if error
-          code = error.elements['code'].text.to_i
-          message = error.elements['description'].text
-          if (1..3).include?(code)
-            raise CredentialsError, message
-          elsif (4..8).include?(code)
-            raise AddressError, message
-          else
-            raise Error, message
+      def check_error(response) #:nodoc:
+        if response.error
+          exception = case response.error.code
+          when 1..3; CredentialsError
+          when 4..8; AddressError
+          else;      Error
           end
+          raise exception, response.error.message
         end
       end
 
---
+++ test/unit/graticule/geocoder/geocoder_ca_test.rb
@@ -0,0 +1,41 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../../test_helper')
+
+module Graticule
+  module Geocoder
+    class GeocoderCaTest < Test::Unit::TestCase
+      
+      def setup
+        URI::HTTP.responses = []
+        URI::HTTP.uris = []
+        
+        @geocoder = GeocoderCa.new
+        @location = Location.new(
+          :latitude => 45.418076,
+          :longitude => -75.693293,
+          :locality => "ottawa",
+          :precision => :unknown,
+          :region => "ON",
+          :street => "200 MUTCALF  "
+        )
+      end
+      
+      def test_success
+        prepare_response(:success)
+        assert_equal @location, @geocoder.locate('200 mutcalf, ottawa on')
+      end
+      
+      def test_url
+        prepare_response(:success)
+        @geocoder.locate('200 mutcalf, ottawa on')
+        assert_equal 'http://geocoder.ca/?geoit=XML&locate=200%20mutcalf,%20ottawa%20on&showpostal=1&standard=1',
+                     URI::HTTP.uris.first
+      end
+
+    protected
+      def prepare_response(id)
+        URI::HTTP.responses << response('geocoder_ca', id)
+      end
+
+    end
+  end
+end
