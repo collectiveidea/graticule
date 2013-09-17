3d9e7667a0f3f18f818a381c5c444d45b0e5217d
Jason Roelofs <jasongroelofs@gmail.com>
2013-09-17 16:04:38 -0400

    Update mapquest handling to use the Community API

--- lib/graticule/geocoder/mapquest.rb
+++ lib/graticule/geocoder/mapquest.rb
@@ -1,21 +1,49 @@
-require 'htmlentities'
-
 # encoding: UTF-8
 module Graticule #:nodoc:
   module Geocoder #:nodoc:
 
-    # Mapquest requires both a client id and a password, which you can
-    # get by registering at:
-    # http://developer.mapquest.com/Home/Register?_devAPISignup_WAR_devAPISignup_action=signup&_devAPISignup_WAR_devAPISignup_clientType=Developer
+    # Mapquest uses the Licenced Community API which requires an api key. You can sign up an account
+    # and get an api key by registering at: http://developer.mapquest.com/
     #
-    # mq = Graticule.service(:mapquest).new(CLIENT_ID, PASSWORD)
+    # mq = Graticule.service(:mapquest).new(API_KEY)
     # location = gg.locate('44 Allen Rd., Lovell, ME 04051')
     # [42.78942, -86.104424]
     #
     class Mapquest < Base
-      # I would link to the documentation here, but there is none that will do anything but confuse you.
 
+      def initialize(api_key)
+        @api_key = api_key
+        @url = URI.parse('http://www.mapquestapi.com/geocoding/v1/address')
+      end
+
+      # Locates +address+ returning a Location
+      def locate(address)
+        get :q => address.is_a?(String) ? address : location_from_params(address).to_s
+      end
+
+      protected
+
+      def make_url(params) #:nodoc
+        request = Mapquest::Request.new(params[:q], @api_key)
+        url = @url.dup
+        url.query = escape(request.query)
+        url
+      end
+
+      class Request
+        def initialize(address, api_key)
+          @address = address
+          @api_key = api_key
+        end
+
+        def query
+          "key=#{@api_key}&outFormat=xml&inFormat=kvp&location=#{URI.escape(@address)}"
+        end
+      end
+
+      # See http://www.mapquestapi.com/geocoding/geocodequality.html#granularity
       PRECISION = {
+        'P1' => Precision::Point,
         'L1' => Precision::Address,
         'I1' => Precision::Street,
         'B1' => Precision::Street,
@@ -31,80 +59,45 @@ module Graticule #:nodoc:
         'A1' => Precision::Country
       }
 
-      def initialize(client_id, password)
-        @password = password
-        @client_id = client_id
-        @url = URI.parse('http://geocode.dev.mapquest.com/mq/mqserver.dll')
-      end
-
-      # Locates +address+ returning a Location
-      def locate(address)
-        get :q => address.is_a?(String) ? address : location_from_params(address).to_s
-      end
-
-      protected
-
-      def make_url(params) #:nodoc
-        request = Mapquest::Request.new(params[:q], @client_id, @password)
-        url = @url.dup
-        url.query = escape(request.query)
-        url
-      end
-
-      class Request
-        def initialize(address, client_id, password)
-          @address = address
-          @client_id = client_id
-          @password = password
-        end
-
-        def query
-          "e=5&<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><Geocode Version=\"1\">#{address_string}#{authentication_string}</Geocode>"
-        end
-
-        def address_string
-          "<Address><Street>#{escaped_address}</Street></Address><GeocodeOptionsCollection Count=\"0\"/>"
-        end
-        
-        def authentication_string
-          "<Authentication Version=\"2\"><Password>#{@password}</Password><ClientId>#{@client_id}</ClientId></Authentication>"
-        end
-
-        def escaped_address
-          HTMLEntities.new.encode(@address, :basic)
-        end
-      end
-
       class Address
         include HappyMapper
-        tag 'GeoAddress'
-        element :latitude, Float, :tag => 'Lat', :deep => true
-        element :longitude, Float, :tag => 'Lng', :deep => true
-        element :street, String, :tag => 'Street'
-        element :locality, String, :tag => 'AdminArea5'
-        element :region, String, :tag => 'AdminArea3'
-        element :postal_code, String, :tag => 'PostalCode'
-        element :country, String, :tag => 'AdminArea1'
-        element :result_code, String, :tag => 'ResultCode'
+        tag 'location'
+        element :latitude, Float, :tag => 'lat', :deep => true
+        element :longitude, Float, :tag => 'lng', :deep => true
+        element :street, String, :tag => 'street'
+        element :locality, String, :tag => 'adminArea5'
+        element :region, String, :tag => 'adminArea3'
+        element :postal_code, String, :tag => 'postalCode'
+        element :country, String, :tag => 'adminArea1'
+        element :result_code, String, :tag => 'geocodeQualityCode'
 
         def precision
           PRECISION[result_code.to_s[0,2]] || :unknown
         end
       end
 
+      class Locations
+        include HappyMapper
+        has_many :addresses, Address, :tag => "location"
+      end
+
       class Result
         include HappyMapper
-        tag 'GeocodeResponse'
-        has_many :addresses, Address, :deep => true
+        has_one :locations, Locations, :tag => "locations"
+      end
+
+      class Response
+        include HappyMapper
+        has_one :result, Result, :deep => true
       end
 
       def prepare_response(xml)
-        Result.parse(xml, :single => true)
+        Response.parse(xml, :single => true)
       end
 
       # Extracts a location from +xml+.
-      def parse_response(result) #:nodoc:
-        addr = result.addresses.first
+      def parse_response(response) #:nodoc:
+        addr = response.result.locations.addresses.first
         Location.new(
           :latitude    => addr.latitude,
           :longitude   => addr.longitude,
