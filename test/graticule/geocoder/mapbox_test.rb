462d2c5f3dc9ebb2539d557a7817b0ca9abc9065
Jason Carpenter <jcarpenter88@mac.com>
2014-04-02 14:22:29 -0400

    Add support for Mapbox

---
+++ test/graticule/geocoder/mapbox_test.rb
@@ -0,0 +1,31 @@
+# encoding: UTF-8
+require 'test_helper'
+
+module Graticule
+  module Geocoder
+    class MapboxTest < Test::Unit::TestCase
+      def setup
+        @geocoder = Mapbox.new("breakthrough.hm6lgdkl")
+      end
+
+      def test_locate_success
+        URI::HTTP.responses << response("mapbox", "success", "json")
+
+        expected = Location.new(
+          :country     => "United States",
+          :latitude    => 37.33054,
+          :longitude   => -122.02912,
+          :street      => "1 Infinite Loop",
+          :locality    => "Cupertino",
+          :region      => "California",
+          :postal_code => "95014",
+          :precision   => :address
+        )
+
+        actual = @geocoder.locate("1 Infinite Loop, Cupertino, CA")
+
+        assert_equal(expected, actual)
+      end
+    end
+  end
+end
